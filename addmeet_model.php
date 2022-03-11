e2cb293261e1a89168a7cf05957574447281b5f7
mydr2m <preedawanworking@gmail.com>
2022-03-11 11:13:22 +0700

    update

--- addmeet_model.php
+++ addmeet_model.php
@@ -12,33 +12,24 @@
 	$addequipment=$_POST['addequipment'];
 	$userid=$_SESSION['userid'];
 	$remark=$_POST['remark'];
-	// $meetfile=$_POST['meetfile'];
+	$meetfile=$_POST['meetfile'];
 
 	// addfile
-	$file=$_FILES['meetfile'];
-	$filename=$_FILES["meetfile"]["name"]; 
-	$filTmpename= $_FILES["meetfile"]["tmp_name"]; 
-	$fileExt= explode(".",$filename);
-	$fileAcExt= strtolower(end($fileExt)); 
-	$newFilename= time() . "." . $fileAcExt; 
-	$fileDes= 'upload/'.$newFilename;
 
-	move_uploaded_file($filTmpename,$fileDes); 
+	$file=$_FILES['meetfile'];
+	$filename=$_FILES["meetfile"]["name"];
+	$filTmpename= $_FILES["meetfile"]["tmp_name"];
+	$fileExt= explode(".",$filename);
+	$fileAcExt= strtolower(end($fileExt));
+	$newFilename= time() . "." . $fileAcExt;
+	$fileDes= 'uploadfile/'.$newFilename;
+
+	move_uploaded_file($filTmpename,$fileDes);
 	$meetfilelocation=$fileDes;
 
 	mysqli_query($conn, "insert into meeting (title, head, numattend, listname, roomid, start, end, addequipment, userid, remark, meetfile) 
 	values ('$title', '$head', '$numattend', '$listname', '$roomid', '$start', '$end', '$addequipment', '$userid', '$remark', '$meetfilelocation')");
 	header('location:addmeet.php');
-
-	// if(mysqli_query($conn, "insert into meeting (title, head, numattend, listname, roomid, start, end, addequipment, userid, remark, meetfile) 
-	// values ('$title', '$head', '$numattend', '$listname', '$roomid', '$start', '$end', '$addequipment', '$userid', '$remark', '$meetfile')"))
-	// {
-	// 	header('location:addmeet.php');
-	// }
-	// else{
-	// 	echo "ERROr <br>";
-	// 	echo $title."<br>". $head."<br>". $numattend."<br>". $listname."<br>". $roomid."<br>". $start."<br>". $end."<br>". $addequipment."<br>". $remark."<br>". $meetfile."<br>". $userid;
-	// }
 
 	// echo "insert into test (title, head, numattend, listname, roomid, start, end, addequipment, userid, remark, meetfile) 
 	// values ('$title', '$head', '$numattend', '$listname', '$roomid', '$start', '$end', '$addequipment', '$userid', '$remark', '$meetfile')";
